604febe4e72aa465c67f6ae1905f97599a93427e
desshub <korirdesmond@MacBook-Pro-3.local>
2020-12-14 23:54:51 +0300

    Define schemas

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './schema';
+import { resolvers } from './resolvers';
 
 const app = express();
 
+app.use(cors())
+
 const port = 3000;
 const path = '/graphql';
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({app, path});
 
 app.listen(port, (): void => console.log(`App running on port ${port}`))
