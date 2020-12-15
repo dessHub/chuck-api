1f0e61574a7adbc353d7bc4b649ea9a78f8f323d
desshub <korirdesmond@MacBook-Pro-3.local>
2020-12-15 21:59:55 +0300

    add env value

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors())
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = '/graphql';
 const server = new ApolloServer({
   typeDefs: schema,
